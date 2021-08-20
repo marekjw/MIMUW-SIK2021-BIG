dd2a6dd0197590afc3bf6554a665a983eff803a7
zofia <marekjwesolowski@gmail.com>
2021-08-20 02:42:16 +0200

    Server Manager created
    server_main now sets up a connection.
    Created new class files

---
+++ server/ServerManager.h
@@ -0,0 +1,6 @@
+#ifndef SERVERMANAGER_H
+#define SERVERMANAGER_H
+
+class ServerManager {};
+
+#endif // SERVERMANAGER_H
--- server/server-main.cpp
+++ server/server-main.cpp
@@ -1,20 +1,24 @@
+#include "../util/err.h"
 #include <cstdlib>
+#include <cstring>
+#include <ctime>
+#include <iostream>
+#include <netdb.h>
+#include <sys/socket.h>
 #include <unistd.h>
 
-#include <ctime>
-
-#include <iostream>
-
 int main(int argc, char **argv) {
-  int port_no = 2021, seed = time(NULL), turning_speed = 6, round_per_sec = 50,
-      width = 640, height = 480;
+  int seed = time(NULL), turning_speed = 6, round_per_sec = 50, width = 640,
+      height = 480;
+
+  std::string port_no{"2021"};
 
   int c;
 
   while ((c = getopt(argc, argv, "p:s:t:v:w:h:")) != -1) {
     switch (c) {
     case 'p':
-      port_no = atoi(optarg);
+      port_no = std::string(optarg);
       break;
     case 's':
       seed = atoi(optarg);
@@ -35,5 +39,21 @@ int main(int argc, char **argv) {
       std::cerr << "Invalid option" << std::endl;
       exit(1);
     }
+  }
+
+  addrinfo *addr, hints;
+  memset(&hints, 0, sizeof hints);
+  hints.ai_protocol = IPPROTO_UDP;
+  hints.ai_flags = AI_PASSIVE;
+
+  int server_socket, status;
+  if ((status = getaddrinfo(nullptr, port_no.data(), &hints, &addr)) != 0) {
+    fprintf(stderr, "getaddrinfo error: %s\n", gai_strerror(status));
+    exit(1);
+  }
+
+  server_socket = socket(addr->ai_family, addr->ai_socktype, addr->ai_protocol);
+  if (bind(server_socket, addr->ai_addr, addr->ai_addrlen) != 0) {
+    syserr("bind error");
   }
 }
